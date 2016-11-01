f7a55559f46f2720ba098cc47687e092c57b10dc
srinidhiprabhu96 <srinidhiprabhu96@gmail.com>
2016-11-01 19:40:31 +0530

    Added function for prediction from words

--- Speech_processing/Main.java
+++ Speech_processing/Main.java
@@ -7,22 +7,20 @@ import java.nio.charset.StandardCharsets;
 
 class Main{
     public static void main(String[] args){
-	String s = "class Factorial{    public static void main(String[] a){       System.out.println();    }}class Fac "; // s should hold the entire code that was generated till now.
+	String s = "class Factorial{    public static void main(String[] a){      "; // s should hold the entire code that was generated till now.
 	String test1 = "sistng"; // This is the token/word received from speech recognition.
-	ArrayList<String> test = new ArrayList<String>();
-	test.add("system");
-	test.add("println");
-	test.add("hello");
-	test.add("string");
-	ArrayList<String> result  = Predict.getNextToken(s);
-	System.out.println(result);
-	System.out.println(Predict.hammingDistance(test, test1));
+	String a = "class name open public static void main open string open close args close open print open open number 1 plus number 10 close close";
+	Predict.generateProcessed(a);
     }
 }
 
-class Predict{
 
-	 public static ArrayList<String> miniJavaTokens = new ArrayList<String>(
+// The class used for processing and predicting.
+class Predict{
+   
+   public static final int hamThreshold = 2; // Hamming distance threshold - might need to be changed later.
+   public static String code = "";
+   public static ArrayList<String> miniJavaTokens = new ArrayList<String>(
     Arrays.asList(
       "(", ")",
       "[", "]",
@@ -33,7 +31,7 @@ class Predict{
       "-","*",
       "/","&&",
       "||","!",
-      "booleaan","class",
+      "boolean","class",
       "interface","else",
       "extends","false",
       "if","while",
@@ -50,7 +48,7 @@ class Predict{
        "open", "close",
        "open", "close",
        "open", "close",
-       "semicolon", "dot",
+       "next", "dot",
        "equal", "less",
        "different", "plus",
        "minus","times",
@@ -68,12 +66,54 @@ class Predict{
        "print","void",
        "define"
      ) );
-
-  //  public static String sommething(ArrayList<String> )
-   public static final int hamThreshold = 2;
-	 public static String getCorrectedToken() {
-				return null;
-		}
+   // Code written by Srinidhi 
+   public static String generateProcessed(String s){
+   	String[] split_tokens;
+   	ArrayList<String> next_tokens;
+   	ArrayList<String> next_processed;
+   	split_tokens = s.split(" ");
+   	String toAdd;
+   	int index;
+   	for(int i=0;i<split_tokens.length;i++){
+   		next_tokens = Predict.getNextToken(code);
+   		next_processed = new ArrayList<String>();
+   		if(split_tokens[i].equals("number")){
+   			code = code + split_tokens[i+1] + " ";
+   			i++;
+   			continue;
+   		}
+   		for(int j=0;j<next_tokens.size();j++){
+   			index = miniJavaTokens.indexOf(next_tokens.get(j));
+   			//System.out.println(index);
+   			if(index != -1)next_processed.add(myJavaTokens.get(index));
+   		}
+   		//System.out.println(next_processed);
+   		if(next_processed.size() != 0){
+   			index = Predict.hammingDistance(next_processed,split_tokens[i]);
+   			toAdd = next_tokens.get(index);
+   		}
+   		else toAdd = split_tokens[i];   		
+   		code = code + toAdd + " ";
+   		
+   	}
+   	//System.out.println(code);
+   	next_tokens = Predict.getNextToken(code);
+   	//System.out.println(next_tokens);
+   	while(next_tokens != null && next_tokens.size() == 1 && !(next_tokens.get(0).equals("<IDENTIFIER>"))){
+   	//System.out.println(next_tokens);
+   		code = code + next_tokens.get(0) + " ";
+   		next_tokens = Predict.getNextToken(code);
+   		//System.out.println(next_tokens);
+   		
+   	}
+   	System.out.println(code);
+   	return null;
+   }
+   
+   // End of code written by Srinidhi
+   
+   
+   // Generates the list of next tokens by generating a parse exception.
    public static ArrayList<String> getNextToken(String code) {
    	String[] result;
    	ArrayList<String> result2 = new ArrayList<String>();
@@ -86,7 +126,7 @@ class Predict{
          result = e.toString().split(":");
          result = result[2].split("...\n");
       	for(String a: result){
-
+		
 		result2.add(a.replaceAll("\\s+","").replaceAll("\n","").replaceAll("\"",""));
 	}
 	result2.remove(result2.size() - 1);
@@ -94,11 +134,12 @@ class Predict{
       }
       return null;
    }
-	public static String hammingDistance(ArrayList<String> predicted, String original){
+	public static int hammingDistance(ArrayList<String> predicted, String original){
 		String result = null;
 		ArrayList<Integer> score = new ArrayList<Integer>();
 		//int min_ind = -1;
 		int min_val = 100;
+		int ret_val = 0;
 		for(String a: predicted){
 			int shorter = Math.min(a.length(), original.length());
     			int longer = Math.max(a.length(), original.length());
@@ -110,12 +151,12 @@ class Predict{
     			val = Predict.LevenshteinDistance(a,original);
     			score.add(val);
     			if(val < min_val){
-    				result = a;
+    				ret_val = predicted.indexOf(a);
 				min_val = val;
 			}
 		}
-		System.out.println(score);
-		return result;
+		//System.out.println(score);
+		return ret_val;
 	}
 	public static int LevenshteinDistance(String x, String y){
 		  int cost;
@@ -137,4 +178,5 @@ class Predict{
 	}
 
 
-}
+} 
+
