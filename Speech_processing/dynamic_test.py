dd8a1160d0b437fae95a37ff005fc53e49386ed0
SaiTeja <saiteja.s@telenniumsolutions.com>
2016-11-08 18:19:47 +0530

    added changes to dynamic test

--- Speech_processing/dynamic_test.py
+++ Speech_processing/dynamic_test.py
@@ -11,43 +11,63 @@ def recognise(source):
 	audio = r.listen(source)
 
 	try:
-	    return r.recognize_google(audio)
+		return r.recognize_google(audio)
 	except sr.UnknownValueError:
-	    return "null"
+		return "null"
 	except sr.RequestError as e:
-	    return "null"
+		return "null"
 
 def foo(window):
-
+	fileObject = open("answers/code.minijava", "w")
 	# obtain audio from the microphone
 	s = ""
 	with sr.Microphone() as source:
 		while True:
 
-		    text = recognise(source)
-		    if text	== "exit":
-		    	break
-		    elif text == "next":
-		    	s = s + "\n"
-		    	window.clear()
-		    	window.addstr(0, 0,s);
-		    	window.refresh()
-
-		    elif "back" in text:
-		    	words = text.split(" ")
-		    	for j in range(0,len(words)):
-		    		if words[j] == "back":
-				    	i = s.rfind(" ")
-				    	s = s[:i]
-		    	window.clear()
-		    	window.addstr(0, 0,s);
-		    	window.refresh()
-
-		    else:
-		    	s = Predict.getWindow(text)
-		    	window.clear()
-		    	window.addstr(0, 0,s);
-		    	window.refresh()
+			text = recognise(source)
+			if text != "null" :
+				if text	== "exit":
+					fileObject.write(s)
+					fileObject.close()		    			    		    	
+					break
+				elif "back" in text:
+					words = text.split(" ")
+					for j in range(0,len(words)):
+						if words[j] == "back":
+							i = s.rfind(" ")
+							s = s[:i]
+					window.clear()
+					window.addstr(0, 0,s);
+					window.refresh()
+				elif "up" in text:
+					words = text.split(" ")
+					for j in range(0,len(words)):
+						if words[j] == "up":
+							i = s.rfind("\n")
+							s=s[:i]
+					window.clear()
+					window.addstr(0, 0,s);
+					window.refresh()
+				elif text == "tab":
+					s = s+"\t"
+					window.clear()
+					window.addstr(0, 0,s);
+					window.refresh()
+				elif text == "":
+					s = s +"" + text
+					window.clear()
+					window.addstr(0, 0,s);
+					window.refresh()
+				elif text =="\n":
+					s = s +"" + text
+					window.clear()
+					window.addstr(0, 0,s);
+					window.refresh()
+				else:
+					s = Predict.getWindow(text)
+					window.clear()
+					window.addstr(0, 0,s);
+					window.refresh()
 
 def main():
 	curses.wrapper(foo)
